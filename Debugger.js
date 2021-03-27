4f5d1efdc97c551648abba1caafa5a5828656cb7
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2021-03-27 15:12:48 -0500

    Update Debugger.js

--- Debugger.js
+++ Debugger.js
@@ -1,1 +1,1 @@
-(h=>{setInterval(h,4e3,(h`0`||h)`1`)})((A,$=($=>$).constructor,i=A=>{A[0]?$`while(1)1`.call``:$`debugger`.call``;i(++A)})=>{try{return+A?i:i(0)}catch{}})
+($=>$=_=>setTimeout(_=>{debugger;$()},4e3))()()
