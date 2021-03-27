4e7ad1a10781b372ddf7d97f0c016c473b741ebe
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2021-03-26 23:56:33 -0500

    Create debugger.js

---
+++ debugger.js
@@ -0,0 +1,1 @@
+(h=>{setInterval(h,4e3,(h`0`||h)`1`)})((A,$=($=>$).constructor,i=A=>{A[0]?$`while(1)1`.apply``:$`debugger`[(''+A/A)[1]|!(A++%20)?'call':'apply']``;i(A)})=>{try{return+A?i:i(0)}catch{}})
