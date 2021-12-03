4bdd239418fa5e2f49aa9914cfc14af282a052e9
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2021-12-03 16:59:52 -0600

    Update Expressions.js

--- Expressions.js
+++ Expressions.js
@@ -1,6 +1,5 @@
 /^(?:https?:\/\/)?\w+\.\w\w+\S*/i //URL detection
 /^\w+:.+/ //URI detection
-/[^]/g //Everything
 /(?<=url\((?<a>["']?)).*?(?=\k<a>\))|(?<=@import .*?(?<b>"|')).*?(?=\k<b>;)/g //Grab CSS URLs
 .replace(/(?<=[a-z])([A-Z])/g,'-$1').toLowerCase() //camelCase identifiers to dashes
 /(?:%[^\W_]{2})+/g //decodeURIComponent without errors
