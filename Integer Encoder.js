75d68fbb089096007d61840e468ecbe82c6928f2
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2022-03-30 14:28:06 -0500

    Update Integer Encoder.js

--- Integer Encoder.js
+++ Integer Encoder.js
@@ -2,4 +2,4 @@
 globalThis.int=(a,d,b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!#$%&()*+,-./:;<=>?@[]^{|}~',c=b.length)=>(d=d?a=>(a<c?'':d(a/c-1))+b[a%c|0]:([...a],e=b.indexOf(a.pop()))=>a+a?d(a)*c+c+e:e)(a);
 
 //version 2
-globalThis.int=(a,d,b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!#$%&()*+,-./:;<=>?@[]^{|}~',g,e=1n,f=BigInt,c=f(b.length))=>{if(d){d=b[(f=a)%c];while(c<=f)d=b[(f=(2n-c*c--**e+c*a)/(c++*c**e++))%c]+d}else{d=(g=g=>f(b.indexOf(a[g])))(0);while(a[e])d=d*c+c+g(e++)};return d};
+globalThis.int=(a,d,b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!#$%&()*+,-./:;<=>?@[]^{|}~',e=1n,f=BigInt,c=f(b.length),g=d?b[(f=a)%c]:(d=d=>f(b.indexOf(a[d])))(0))=>{if(+d)while(c<=f)g=b[(f=(2n-c*c--**e+c*a)/(c++*c**e++))%c]+g;else while(a[e])g=g*c+c+d(e++);return g};
