7663584d282047e61f9b4c2a92e29f85d58da3d5
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2022-04-01 16:17:01 -0500

    Update Integer Encoder.js

--- Integer Encoder.js
+++ Integer Encoder.js
@@ -3,3 +3,6 @@ globalThis.int=(a,d,b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU
 
 //version 2
 globalThis.int=(a,d,b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!#$%&()*+,-./:;<=>?@[]^{|}~',e=1n,f=BigInt,c=f(b.length),g=d?b[(f=a)%c]:(d=d=>f(b.indexOf(a[d])))(0))=>{if(+d)while(c<=f)g=b[(f=(2n-(e*=c)*c--+c*a)/(c++*e))%c]+g;else while(a[e])g=g*c+c+d(e++);return g};
+
+//generalized encoder
+globalThis.enc=(a,d,f=BigInt,i=(a,b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!$&()*+,-./:;<=>?@[]^{|}~',e=1n,c=f(b.length),g=d?(d=d=>f(b.indexOf(a[d])))(0):b[(f=a)%c])=>{if(d)while(a[e])g=g*c+c+d(e++);else while(c<=f)g=b[(f=(2n-(e*=c)*c--+c*a)/(c++*e))%c]+g;return g})=>d?(i(a)+'').replace(/[^]{1,7}(?=(?:[^]{7})*$)/g,a=>String.fromCodePoint(a)):i(f(a.replace(/[^]/g,a=>'0'.repeat(7-(a=''+a.codePointAt()).length)+a)));
