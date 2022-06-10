03f5f0a75052cd84b9b9d1491c5186447835c079
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2022-06-10 15:06:27 -0500

    Update Expressions.js

--- Expressions.js
+++ Expressions.js
@@ -6,7 +6,7 @@
 /*
 
 all one-byte characters, excluding "'`\n\ for encoding
-\x00\x01\x02\x03\x04\x05\x06\x07\b\t\n\v\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~\x7F
+\x00\x01\x02\x03\x04\x05\x06\x07\b\t\v\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~\x7F
 
 */
 //All-in-one Discord phishing filter
